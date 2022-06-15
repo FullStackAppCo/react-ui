2f1081c0210d15b62bbd9787972490a84643ade3
Joby Harding <code@fullstackapp.co>
2022-06-15 21:20:03 +0100

    Allow IconHeading icon size customisation

--- dist/type/IconHeading.d.ts
+++ dist/type/IconHeading.d.ts
@@ -1,8 +1,10 @@
 import { HTMLProps, ReactNode } from 'react';
 import { IconProp } from "@fortawesome/fontawesome-svg-core";
-interface FooterHeadingProps extends HTMLProps<Element> {
+interface IconHeadingProps extends HTMLProps<Element> {
     children: ReactNode;
+    className?: string;
     icon: IconProp;
+    iconSize?: number;
 }
-export declare function IconHeading({ children, icon }: FooterHeadingProps): JSX.Element;
+export declare function IconHeading({ children, className, icon, iconSize }: IconHeadingProps): JSX.Element;
 export {};
--- src/type/IconHeading.tsx
+++ src/type/IconHeading.tsx
@@ -3,16 +3,18 @@ import {FontAwesomeIcon} from "@fortawesome/react-fontawesome"
 import {IconProp} from "@fortawesome/fontawesome-svg-core";
 import {Heading} from './Heading'
 
-interface FooterHeadingProps extends HTMLProps<Element> {
+interface IconHeadingProps extends HTMLProps<Element> {
   children: ReactNode,
+  className?: string,
   icon: IconProp,
+  iconSize?: number,
 }
 
-export function IconHeading({ children, icon }: FooterHeadingProps) {
+export function IconHeading({children, className = '', icon, iconSize = 4}: IconHeadingProps) {
   return (
     <div className="flex items-center space-x-2">
-      <FontAwesomeIcon icon={icon} className="text-gray-400 dark:text-gray-500" />
-      <Heading>{ children }</Heading>
+      <FontAwesomeIcon icon={icon} className={`w-${iconSize} h-${iconSize} text-gray-400 dark:text-gray-500`} />
+      <Heading className={className}>{children}</Heading>
     </div>
   )
 }
